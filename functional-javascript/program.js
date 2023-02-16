c79337a63b89e2d06dbdf7321518c55a883292de
DamyanYara <124031495+DamyanYara@users.noreply.github.com>
2023-02-16 17:53:48 +0200

    some of the tasks

---
+++ functional-javascript/program.js
@@ -0,0 +1,48 @@
+//Task 1
+// function upperCaser(input) {
+//     return input.toUpperCase();
+// }
+
+// module.exports = upperCaser
+
+//Task 2
+// function repeat (operation, num){
+//    for(let i = 0; i<num; i++){
+//     operation()
+//    }
+// }
+// module.exports = repeat
+
+//Task 3
+// function doubleAll(numbers){
+//     let arr = numbers.slice()
+//     const map1 = arr.map(x => x*2)
+//     return map1
+
+// }
+// module.exports = doubleAll
+
+//Task 4
+
+// function getShortMessages(messages){
+//     messages
+//     return messages.filter(obj => obj.message.length <= 50).map(obj => obj.message);
+    
+// }
+// module.exports = getShortMessages
+
+//Task 5
+// function checkUsersValid(goodUsers){
+//     return function allUsersValid(submittedUsers){
+//         return submittedUsers.every(function(submittedUsers){
+//             return goodUsers.some(function(goodUsers){
+//                 return goodUsers.id === submittedUsers.id;
+//             });
+//         });
+//     }
+// }
+
+// module.exports = checkUsersValid
+
+//Task 6
+
